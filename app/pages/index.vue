bb229f2d7faa4c70a5ea5e442a67af944f74bc14
Tanay Karnik <tanayvk@gmail.com>
2025-03-20 21:21:54 +0530

    init

---
+++ app/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <UContainer>
+      <h1>Small Bets</h1>
+    </UContainer>
+  </div>
+</template>
+
+<script setup>
+definePageMeta({
+  colorMode: "light",
+});
+</script>
